dac0d0e7798a44fe6b63e8482a18f4933a1970c5
Gabriel <gabrielcpf2997@gmail.com>
2021-12-27 21:22:35 -0800

    first commit, basic files

---
+++ app.js
@@ -0,0 +1,54 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const compression = require('compression');
+const helmet = require('helmet');
+const bcrypt = require('bcrypt');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const indexRouter = require('./routes/index');
+
+const app = express();
+
+// mongodb setup
+const mongoose = require('mongoose');
+const mongoDB = `mongodb+srv://admin:${process.env.DB_PASSWORD}@cluster0.92d3a.mongodb.net/cloneBookDB?retryWrites=true&w=majority`;
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(compression());
+app.use(helmet());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
